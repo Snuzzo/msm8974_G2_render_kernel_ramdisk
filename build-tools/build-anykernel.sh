38c92cb9bbb2aaca75c09ae9571d51f351463421
Snuzzo <jrracinfan@gmail.com>
2015-05-27 18:10:25 -0400

    Start working on anykernel

--- build-tools/build-anykernel.sh
+++ build-tools/build-anykernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+BUILD_TOP=$PWD
 #
 #  Build Script for Render Kernel for OPO!
 #  Based off AK'sbuild script - Thanks!
@@ -17,45 +17,127 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="zImage"
 DTBIMAGE="dtb"
-DEFCONFIG="render_defconfig"
 
 # Kernel Details
 VER=Render-Kernel
 
 # Vars
 export LOCALVERSION=~`echo $VER`
-export CROSS_COMPILE=${HOME}/android/source/toolchains/UBER-arm-eabi-4.9-cortex-a15-05132015/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=RenderBroken
 export KBUILD_BUILD_HOST=RenderServer.net
 export CCACHE=ccache
+export CROSS_COMPILE=$BUILD_TOP/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
+
 
 # Paths
-KERNEL_DIR=`pwd`
-REPACK_DIR="${HOME}/android/source/kernel/OPO-AnyKernel"
-PATCH_DIR="${HOME}/android/source/kernel/OPO-AnyKernel/patch"
-MODULES_DIR="${HOME}/android/source/kernel/OPO-AnyKernel/modules"
-ZIP_MOVE="${HOME}/android/source/zips/opo-zips"
-ZIMAGE_DIR="${HOME}/android/source/kernel/msm8974_OPO_render_kernel/arch/arm/boot"
-VARIANT="OPO"
+KERNEL_DIR=$BUILD_TOP/source
+REPACK_DIR=$BUILD_TOP/out/g2/ozip
+ZIMAGE_DIR=$BUILD_TOP/source/arch/arm/boot
+OUT_DIR=$BUILD_TOP/zips/g2-zips
+buildtools=$BUILD_TOP/ramdisk/build-tools
+MODULES_DIR=$REPACK_DIR/system/lib/modules
+
+
+function lunch(){
+mkdir -p $MODULES_DIR
+cp -r $buildtools/ozip/* $REPACK_DIR
+echo "Pick variant..."
+select choice in d800 d801 d802 d803 ls980 vs980 f320x l01f
+do
+case "$choice" in
+	"d800")
+		variant="d800"
+		config="d800_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/d800.lz4
+		break;;
+	"d801")
+		variant="d801"
+		config="d801_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/d801.lz4
+		break;;
+	"d802")
+		variant="d802"
+		config="d802_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/d802.lz4
+		break;;
+	"d803")
+		variant="d803"
+		config="d803_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/d803.lz4
+		break;;
+	"ls980")
+		variant="ls980"
+		config="ls980_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/ls980.lz4
+		break;;
+	"vs980")
+		variant="vs980"
+		config="vs980_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/vs980.lz4
+		break;;
+	"f320x")
+		variant="f320x"
+		config="f320x_defconfig"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk/f320x.lz4
+		break;;
+	"l01f")
+		variant="l01f"
+		config="l01f_defconfig"
+#		ramdisk=$BUILD_TOP/ramdisk/ramdisk/f320x.lz4
+		break;;
+esac
+done
+
+echo "Panel variant..."
+select panel in lgd jdi
+do
+case "$panel" in
+	"lgd")
+		cmdline="console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd androidboot.selinux=permissive"
+		break;;
+	"jdi")
+		cmdline="console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd androidboot.selinux=permissive"
+		break;;
+esac
+done
+
+echo "Pick target..."
+select target in lg aosp
+do
+case "$target" in
+	"lg")
+		# Already set
+		rdflag=1
+		rom="LG"
+		break;;
+	"aosp")
+		rdflag=2
+		rom="LP"
+		ramdisk=$BUILD_TOP/ramdisk/ramdisk_android_L
+		break;;
+esac
+done
+}
 
 # Functions
 function clean_all {
 		rm -rf $MODULES_DIR/*
-		cd $REPACK_DIR
+		cd $ZIP_MOVE
 		rm -rf $KERNEL
 		rm -rf $DTBIMAGE
 		cd $KERNEL_DIR
 		echo
 		make clean && make mrproper
+		cd $BUILD_TOP
 }
 
 function make_kernel {
-		echo
-		make $DEFCONFIG
-		make $THREAD
-		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR
+		cd $KERNEL_DIR
+		make $config > /dev/null
+		time make $THREAD > /dev/null
+		cp -vr $ZIMAGE_DIR/zImage $BUILD_TOP/out/g2/ozip/
 }
 
 function make_modules {
@@ -64,14 +146,16 @@ function make_modules {
 }
 
 function make_dtb {
-		$REPACK_DIR/tools/dtbToolCM -2 -o $REPACK_DIR/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm/boot/
+		$buildtools/dtbTool -s 2048 -o $BUILD_TOP/out/g2/dt.img $BUILD_TOP/source/arch/arm/boot/
 }
 
 function make_zip {
 		cd $REPACK_DIR
-		zip -r9 RenderKernel-CM12_"$VARIANT"-R.zip *
-		mv RenderKernel-CM12_"$VARIANT"-R.zip $ZIP_MOVE
-		cd $KERNEL_DIR
+		zip -r9 RenderKernel-CM12_"$variant"-R.zip *
+		mv RenderKernel-CM12_"$variant"-R.zip $OUT_DIR
+		cd $OUT_DIR
+		md5sum RenderKernel-CM12_"$variant"-R.zip > RenderKernel-CM12_"$variant"-R.zip.md5
+		cd $BUILD_TOP
 }
 
 
@@ -107,6 +191,7 @@ while read -p "Do you want to build kernel (y/n)? " dchoice
 do
 case "$dchoice" in
 	y|Y)
+		lunch
 		make_kernel
 		make_dtb
 		make_modules
